0df728298576df72817471c31cf25f1b25968b30
rheintzmann <heintzmann@gmail.com>
2021-08-02 11:54:43 +0200

    added Aberrations

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -1,20 +1,21 @@
 using Test
 using Random
-
-using PSFs
 using NDTools
 using BenchmarkTools
 using FFTW
 
+using PSFs
+
 pp = PSFParams(580.0, 1.4, 1.518; pol=pol_scalar,method=PSFs.MethodSincR, FFTPlan=nothing)
-sz = (256,256,32)
+sz = (256,256,64)
 sampling=(80,80,100)
 
 @benchmark p = psf(sz,$pp, sampling=sampling,use_resampling=true)
 # (with extra border): 397 ms using standard heuristics
 # Old Data (without extra border): 303 ms -> 0.145 µs / voxel  =  0.304 / prod(sz) * 1e6
 
-ppm = PSFParams(580.0, 1.4, 1.518;pol=pol_scalar,method=PSFs.MethodSincR, FFTPlan=FFTW.MEASURE)
+aberr = PSFs.Aberrations([Zernike_Spherical],[0.1])
+ppm = PSFParams(580.0, 1.4, 1.518;pol=pol_scalar,method=PSFs.MethodSincR, aberrations= aberr, FFTPlan=FFTW.MEASURE)
 p = psf(sz, ppm, sampling=sampling,use_resampling=true); # just to ensure that the measure is already executed
 
 @benchmark p = psf(sz,$pp, sampling=sampling,use_resampling=true)
