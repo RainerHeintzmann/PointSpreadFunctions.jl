e1b4233c616fd3610764d310312c61eefd0e99a0
RainerHeintzmann <heintzmann@gmail.com>
2022-07-19 13:12:44 +0200

    added MethodSTED

--- src/PointSpreadFunctions.jl
+++ src/PointSpreadFunctions.jl
@@ -6,7 +6,7 @@ export PSFParams, sinc_r, jinc_r_2d, pupil_xyz, apsf, psf, k0, kxy, aplanatic_fa
 export get_Abbe_limit, get_Nyquist_limit, get_pupil_aperture
 export kz_mid_pos
 
-export ModeWidefield, ModeConfocal, Mode4Pi, ModeISM, Mode2Photon
+export ModeWidefield, ModeConfocal, Mode4Pi, ModeISM, Mode2Photon, ModeSTED
 export MethodRichardsWolf, MethodPropagate, MethodPropagateIterative, MethodShell, MethodSincR
 
 include("aplanatic.jl")
@@ -73,10 +73,9 @@ end
 + `sampling=nothing`:   The sampling parameters of the resulting PSF.
 + `use_resampling=true`: Exploits a calculation trick, which first calculates the individual PSFs on a twice coarser grid in XY and Z and then upsamples the result. Note that this may be inappropriate due to undersampling due to the Stokes shift which is neglected here. But warnings will then result during the calculations of the subsampled widefield PSFs.
 + `return_amp=false`:    Has to be `false` since confocal amplitude spread functions do not exist for non-zero pinhole sizes. 
-+ `pinhole_positions=[(0.0,0.0)]`:  A list of pinhole positions. One PSF will be returned for each pinhole position. If only a single pinhole position is supplied the PSF will directly be returned instead of a vector of PSFs.
++ `pinhole_positions=[(0.0,0.0)]`:  A list of pinhole positions. One PSF will be returned for each pinhole position. If only a single pinhole position is supplied the PSF will directly be returned instead of a vector of PSFs. Specifies the precise position(s) of the pinholes in the detection path. This allows to simulate an offset (misadjusted) pinhole, or (as a vector of tuples) a PSF for a whole set of positions. See the `psf(ModeISM, ...)` for more details. 
 + `pinhole_ft=disc_pinhole_ft`:  Specifies which function is used to calculate the Fourier transform of the pinhole. This allows the user to control the pinhole shape. 
-+ `pinhole_positions=[(0.0,0.0)]`:  Specifies the precise position(s) of the pinholes in the detection path. This allows to simulate an offset (misadjusted) pinhole, or (as a vector of tuples) a PSF for a whole set of positions. See the `psf(ModeISM, ...)` for more details. 
-+ `ex2p`:   If `true`, the excitation PSF is locally squared to calculate a two-photon confocal PSF. However, you should typically use the `Mode2Photon` to do this. Note that the order of the PSFParams are reversed.
++ `ex_modifier`:   A function that can modify the excitation PSF. By default the identity is used, but other options are `modify_square` to calculate a two-photon confocal PSF. However, you should typically use the `Mode2Photon` to do this. Note that the order of the PSFParams are reversed.
 
 ```jdoctest
 julia> pp_em = PSFParams(0.5,1.4,1.52; mode=ModeConfocal);
@@ -84,7 +83,7 @@ julia> pp_ex = PSFParams(pp_em; λ=0.488, aplanatic=aplanatic_illumination);
 julia> p_conf = psf((128,128,128),pp_em; pp_ex=pp_ex, pinhole=0.1, sampling=(0.040,0.040,0.100));
 ```
 """
-function psf(::Type{ModeConfocal}, sz::NTuple, pp_em::PSFParams; sampling=nothing, pp_ex=nothing, use_resampling=true, pinhole=nothing, pinhole_ft=disc_pinhole_ft,  return_amp=nothing, pinhole_positions=[(0.0,0.0)], ex2p=false) # unclear why the resampling seems to be so bad
+function psf(::Type{ModeConfocal}, sz::NTuple, pp_em::PSFParams; sampling=nothing, pp_ex=nothing, use_resampling=true, pinhole=nothing, pinhole_ft=disc_pinhole_ft,  return_amp=nothing, pinhole_positions=[(0.0,0.0)], ex_modifier=modify_ident) # unclear why the resampling seems to be so bad
     if isnothing(pp_ex) 
         error("The named parameter `pp_ex` is obligatory for confocal calculation. Provide the excitation PSF parameters here.")
     end
@@ -111,7 +110,7 @@ function psf(::Type{ModeConfocal}, sz::NTuple, pp_em::PSFParams; sampling=nothin
     end
 
     # apply the two-photon effect for excitation if needed.
-    psf_ex = ex2p ? abs2.(psf_ex) : psf_ex
+    psf_ex =  ex_modifier(psf_ex) 
 
     # pp_em = PSFParams(pp, mode = ModeWidefield)
     psf_em = let
@@ -124,7 +123,54 @@ function psf(::Type{ModeConfocal}, sz::NTuple, pp_em::PSFParams; sampling=nothin
     end
 
     pinhole_pix = pinhole_AU_to_pix(sz, pp_em, sampling, pinhole)
-    return confocal_int(psf_ex, psf_em, pp_em; pinhole_pix=pinhole_pix, pinhole_ft=pinhole_ft, pinhole_positions=pinhole_positions, ex2p=ex2p)
+    return confocal_int(psf_ex, psf_em, pp_em; pinhole_pix=pinhole_pix, pinhole_ft=pinhole_ft, pinhole_positions=pinhole_positions, ex_modifier=ex_modifier)
+end
+
+"""
+    modify_ident(h)
+
+A function from a series of modficator functions which serve to modify the excitation intensity.
+    This particular version does nothing. It just returns the input `h`.
+"""
+function modify_ident(h)
+    h
+end
+
+"""
+    modify_square(h)
+
+A function from a series of modficator functions which serve to modify the excitation intensity.
+    This version returns the absolute square of the input `h`. It is useful for two-photon microscopy. 
+"""
+function modify_square(h)
+    abs2.(h)
+end
+
+"""
+    modify_STED_exp(h, h_doughnut)
+
+A function from a series of modficator functions which serve to modify the excitation intensity.
+    This version applies a second STED PSF `h_doughnut` to deplete the excitation PSF `h`.
+    The depletion is modeled by an exponential decay as obtained by a relatively short STED pulse following a very short excitation pulse.
+    Note the `h_doughnut` should be the depletion PSF. Note that this function should be used as a tool to define a new function with fixed second argument.
+"""
+function modify_STED_exp(h, h_doughnut)
+    h .* exp.(.- h_doughnut)
+end
+
+"""
+    modify_STED_hyper(h, h_doughnut)
+
+A function from a series of modficator functions which serve to modify the excitation intensity.
+    This version applies a second STED psf `h_doughnut` to deplete the excitation psf `h`.
+    The depletion is modeled by an hyperbolic saturation curve based on anaylsing the steady-state kinetics.
+    This is mostly suitable for CW-STED.
+    Note the `h_doughnut` should be the depletion PSF. Note that this function should be used as a tool to define a new function with fixed second argument.
+    The normalizations are such that `h` as well as `h_doughnut` reache 50% saturation at `h==1.0`. This means that typically your want to use
+    `max(h) << 1` and `max(h_doughnut) >> 1`
+"""
+function modify_STED_hyper(h, h_doughnut)
+    h ./  (one(eltype(h)) .+ h .+ h_doughnut)
 end
 
 """
@@ -149,11 +195,11 @@ function pinhole_AU_to_pix(sz, pp_em, sampling, pinhole)
 end
 
 """
-    confocal_int(psf_ex, psf_em, pinhole=nothing, pinhole_ft=disc_pinhole_ft, sampling=nothing, use_resampling=true, return_amp=nothing, pinhole_positions=[(0.0,0.0)], ex2p=false)
+    confocal_int(psf_ex, psf_em, pinhole=nothing, pinhole_ft=disc_pinhole_ft, sampling=nothing, use_resampling=true, return_amp=nothing, pinhole_positions=[(0.0,0.0)], ex_modifier=modify_ident)
 
 helper function to calculate the confocal detection, once the excitation and emission PSFs and pinhole parameters are defined. Two photon excitation and ISM detection are also included.
 """
-function confocal_int(psf_ex, psf_em, pp_em;  pinhole_pix=nothing, pinhole_ft=disc_pinhole_ft, pinhole_positions=[(0.0,0.0)], ex2p=false)
+function confocal_int(psf_ex, psf_em, pp_em;  pinhole_pix=nothing, pinhole_ft=disc_pinhole_ft, pinhole_positions=[(0.0,0.0)], ex_modifier=modify_ident)
    
     # This can be done a lot more efficiently by staying in Fourier space. Ideally even by only calculating half the range of the jinc function:
     # pinhole = real.(ift2d(jinc_r_2d(sz, pinhole .* AU_pix, pp_em.dtype)))
@@ -162,10 +208,8 @@ function confocal_int(psf_ex, psf_em, pp_em;  pinhole_pix=nothing, pinhole_ft=di
     all_PSFs = [];
     sz = size(psf_em)
 
-    # apply two-photon effect
-    if ex2p
-        psf_ex = abs2.(psf_ex)
-    end
+    # apply two-photon or STED effect 
+    psf_ex = ex_modifier(psf_ex)
 
     rfft_psf_em = rfft2d(psf_em)
     for p in pinhole_positions
@@ -237,7 +281,7 @@ function ism_positions_rect(pinhole_dist, pinhole_grid)
 end
 
 """
-    psf(::Type{ModeISM}, sz::NTuple, pp_em::PSFParams; pinhole=nothing, sampling=nothing, pinhole_ft=box_pinhole_ft, pinhole_positions=nothing, pinhole_dist=0.5, pinhole_grid=(5,5), ism_pos=ism_positions_rect, args...)
+    psf(::Type{ModeISM}, sz::NTuple, pp_em::PSFParams; pinhole=nothing, sampling=nothing, pinhole_ft=box_pinhole_ft, pinhole_positions=[(0.0,0.0)], pinhole_dist=0.5, pinhole_grid=(5,5), ism_pos=ism_positions_rect, args...)
     Calculates a confocal point spread function. The normalisation is such that a completely open `pinhole` diameter yields the excitation PSF with its normalization. 
     Returns the PSF or a vector of PSFs.
     
@@ -292,7 +336,7 @@ end
 + `sampling=nothing`:   The sampling parameters of the resulting PSF.
 + `use_resampling=true`: Exploits a calculation trick, which first calculates the individual PSFs on a twice coarser grid in XY and Z and then upsamples the result. Note that this may be inappropriate due toundersampling due to the Stokes shift which is neglected here. But warnings will then result during the calculations of the subsampled widefield PSFs.
 + `return_amp=false`:    Has to be `false` since confocal amplitude spread functions do not exist for non-zero pinhole sizes. 
-+ `pinhole_positions=nothing`:  A list of pinhole positions. One PSF will be returned for each pinhole position. If only a single pinhole position is supplied the PSF will directly be returned instead of a vector of PSFs.
++ `pinhole_positions=[(0.0,0.0)]`:  A list of pinhole positions. One PSF will be returned for each pinhole position. If only a single pinhole position is supplied the PSF will directly be returned instead of a vector of PSFs.
 
 ```jdoctest
 julia> sz=(128,128,128); sampling = (0.04,0.04,0.120)
@@ -300,7 +344,7 @@ julia> pp_ex = PSFParams(0.8,1.4,1.52; mode=Mode2Photon, aplanatic= aplanatic_il
 julia> p_2p = psf(sz,pp_ex; sampling=sampling);
 ```
 """
-function psf(::Type{Mode2Photon}, sz::NTuple, pp::PSFParams; sampling=nothing, pinhole=nothing, pinhole_ft=disc_pinhole_ft, pinhole_positions=nothing, args...)
+function psf(::Type{Mode2Photon}, sz::NTuple, pp::PSFParams; sampling=nothing, pinhole=nothing, pinhole_ft=disc_pinhole_ft, pinhole_positions=[(0.0,0.0)], args...)
     if isnothing(pinhole) 
         return abs2.(psf(ModeWidefield, sz, pp; sampling=sampling))
     else
@@ -309,7 +353,47 @@ function psf(::Type{Mode2Photon}, sz::NTuple, pp::PSFParams; sampling=nothing, p
 end
 
 """
-    psf(::Type{Mode4Pi}, sz::NTuple, pp_ex::PSFParams; sampling=nothing, pp_ex2=pp_ex, pp_em=nothing, pp_em2=nothing, rel_ex_phase = 0.0, rel_em_phase = 0.0, pinhole=nothing, pinhole_ft=disc_pinhole_ft, pinhole_positions=[(0.0,0.0)], ex2p=false)
+    psf(::Type{ModeSTED}, sz::NTuple, pp_ex::PSFParams, pp_sted::PSFParams; pp_em=nothing, pinhole=nothing, sampling=nothing, pinhole_ft=disc_pinhole_ft, args...)
+    Calculates a 2-photon (potentially confocal) point spread function.  
+    Returns the PSF or a vector of PSFs.
+    
+#Parameters
++ `sz`:         size tuple of the final PSF
++ `pp_ex`:      PSF parameters of the excitation PSF. This should include the exission wavelength (typically in the IR region). Please make sure to also set `pp.aplanatic=aplanatic_illumination`.
++ `pp_sted`:      PSF parameters of the excitation PSF. This should include the exission wavelength (typically in the IR region). Please make sure to also set `pp.aplanatic=aplanatic_illumination`.
++ `pp_em`:      PSF parameters of the emission PSF. This only needs to be supplied, if a pinhole is used. Otherwise non-descanned detection (NDD) is assumed.
++ `pinhole=nothing`:   If `nothing`, NDD is assumed and the PSF is only the square of the excitation PSF. The diameter of each pinhole in Airy Units (AU = 1.22 λ/NA). 
++ `sampling=nothing`:   The sampling parameters of the resulting PSF.
++ `use_resampling=true`: Exploits a calculation trick, which first calculates the individual PSFs on a twice coarser grid in XY and Z and then upsamples the result. Note that this may be inappropriate due toundersampling due to the Stokes shift which is neglected here. But warnings will then result during the calculations of the subsampled widefield PSFs.
++ `return_amp=false`:    Has to be `false` since confocal amplitude spread functions do not exist for non-zero pinhole sizes. 
++ `pinhole_positions=[(0.0,0.0)]`:  A list of pinhole positions. One PSF will be returned for each pinhole position. If only a single pinhole position is supplied the PSF will directly be returned instead of a vector of PSFs.
+
+```jdoctest
+julia> sz=(128,128,128); sampling = (0.04,0.04,0.120)
+julia> pp_em = PSFParams(0.510,1.4,1.52; mode=ModeSTED); # STED suppression beam
+julia> pp_ex = PSFParams(0.488,1.4,1.52; aplanatic= aplanatic_illumination);
+julia> pp_sted = PSFParams(0.570,1.4,1.52; method=MethodPropagateIterative, aplanatic= aplanatic_illumination, pol=pol_circ_spiral);
+julia> p_sted_01 = psf(sz, pp_em; pp_ex=pp_ex, pinhole= 2.0, sampling=sampling, pp_sted=pp_sted, rel_sted_intensity=0.1);
+julia> p_sted_1 = psf(sz, pp_em; pp_ex=pp_ex, pinhole= 2.0, sampling=sampling, pp_sted=pp_sted, rel_sted_intensity=1.0);
+julia> p_sted_5 = psf(sz, pp_em; pp_ex=pp_ex, pinhole= 2.0, sampling=sampling, pp_sted=pp_sted, rel_sted_intensity=5.0);
+julia> pp_sted_b = PSFParams(0.570,1.4,1.52; method=MethodPropagateIterative, aplanatic= aplanatic_illumination, pol=pol_circ_tophat);
+julia> p_sted_b5 = psf(sz, pp_em; pp_ex=pp_ex, pinhole= 2.0, sampling=sampling, pp_sted=pp_sted_b, rel_sted_intensity=5.0);
+julia> using View5D; @vt p_sted_01 p_sted_1 p_sted_5 p_sted_b5
+```
+"""
+function psf(::Type{ModeSTED}, sz::NTuple, pp::PSFParams; sampling=nothing, pp_sted::PSFParams, rel_sted_intensity = 1.0, pinhole=nothing, pinhole_ft=disc_pinhole_ft, pinhole_positions=[(0.0,0.0)], modify_STED=modify_STED_exp, ex_modifier=modify_ident, args...)
+
+    h_STED = psf(ModeWidefield, sz, pp_sted; sampling=sampling)
+    h_STED = rel_sted_intensity .* h_STED ./ maximum(h_STED)
+    # return h_STED
+
+    ex_modifier_new = (x) -> modify_STED(ex_modifier(x), h_STED)
+
+    psf(ModeConfocal, sz, pp; ex_modifier=ex_modifier_new, pinhole=pinhole, sampling=sampling, pinhole_ft=pinhole_ft, pinhole_positions=pinhole_positions, args...) # confocal is able to handle this well
+end
+
+"""
+    psf(::Type{Mode4Pi}, sz::NTuple, pp_ex::PSFParams; sampling=nothing, pp_ex2=pp_ex, pp_em=nothing, pp_em2=nothing, rel_ex_phase = 0.0, rel_em_phase = 0.0, pinhole=nothing, pinhole_ft=disc_pinhole_ft, pinhole_positions=[(0.0,0.0)], ex_modifier=modify_ident)
     Calculates a 4Pi point spread function. 
     Returns the PSF or a vector of PSFs.
     
@@ -317,27 +401,27 @@ end
 + `sz`:         size tuple of the final PSF
 + `pp_ex`:      PSF parameters of the excitation PSF. This should include the exission wavelength (typically in the IR region). Please make sure to also set `pp.aplanatic=aplanatic_illumination`.
 + `pp_ex2`:     PSF parameters of the other side excitation PSF. If `nothing` is provided, single-sided excitation (e.g. 4Pi Type B) is assumed.
-+ `ex2p=true`:  If `true`, two-photon exciation is assumed.
++ `ex_modifier=modify_square`:  If `modify_square`, two-photon exciation is assumed.
 + `pp_em`:      PSF parameters of the emission PSF. This only needs to be supplied, if a pinhole is used.
 + `pp_em2`:     PSF parameters of the other side emission PSF. If `nothing is supplied, Type A 4Pi microscopy is assumed with single-sided (or incoherent) detection.`
 + `pinhole=nothing`:   If `nothing`, NDD is assumed and the PSF is only the square of the excitation PSF. The diameter of each pinhole in Airy Units (AU = 1.22 λ/NA). 
 + `sampling=nothing`:   The sampling parameters of the resulting PSF.
-+ `pinhole_positions=nothing`:  A list of pinhole positions. One PSF will be returned for each pinhole position. If only a single pinhole position is supplied the PSF will directly be returned instead of a vector of PSFs.
++ `pinhole_positions=[(0.0,0.0)]`:  A list of pinhole positions. One PSF will be returned for each pinhole position. If only a single pinhole position is supplied the PSF will directly be returned instead of a vector of PSFs.
 
 ```jdoctest
 julia> sampling = (0.04,0.04,0.04)
 julia> sz = (128,128,128)
 julia> pp_em = PSFParams(0.5,1.3,1.52; mode=Mode4Pi, pol=pol_x);
 julia> pp_ex = PSFParams(0.800,1.3,1.52; aplanatic=aplanatic_illumination, mode=Mode4Pi, pol=pol_x);
-julia> @time p_4pi = psf(sz,pp_ex; pp_ex2=pp_ex, pp_em=pp_em, pp_em2=pp_em, sampling=sampling, pinhole=2.0, ex2p=true);
+julia> @time p_4pi = psf(sz,pp_ex; pp_ex2=pp_ex, pp_em=pp_em, pp_em2=pp_em, sampling=sampling, pinhole=2.0, ex_modifier=modify_square);
 ```
 """
-function psf(::Type{Mode4Pi}, sz::NTuple, pp_ex::PSFParams; sampling=nothing, pp_ex2 = pp_ex, pp_em=nothing, pp_em2=nothing, rel_ex_phase = 0.0, rel_em_phase = 0.0, pinhole=nothing, pinhole_ft=disc_pinhole_ft, pinhole_positions=[(0.0,0.0)], ex2p=false)
+function psf(::Type{Mode4Pi}, sz::NTuple, pp_ex::PSFParams; sampling=nothing, pp_ex2 = pp_ex, pp_em=nothing, pp_em2=nothing, rel_ex_phase = 0.0, rel_em_phase = 0.0, pinhole=nothing, pinhole_ft=disc_pinhole_ft, pinhole_positions=[(0.0,0.0)], ex_modifier=modify_square)
     if isnothing(pp_ex)
         error("For a 4Pi PSF, you have to at least provide one excitation psf using the named argument `pp_ex`.")
     end
     
-    if ex2p
+    if ex_modifier == modify_square # a special case that warrants the resampling trick twice
         Nq_ex = (pp_ex.λ/2) /pp_ex.NA / 2
         Nq_em = pp_em.λ /pp_em.NA / 2
         Nq_XY = 1/(1/Nq_ex + 1/Nq_em)
@@ -389,7 +473,7 @@ function psf(::Type{Mode4Pi}, sz::NTuple, pp_ex::PSFParams; sampling=nothing, pp
     end
 
     pinhole_pix = pinhole_AU_to_pix(sz, pp_em, sampling, pinhole)
-    return confocal_int(psf_ex, psf_em, pp_em; pinhole_pix=pinhole_pix, pinhole_ft=pinhole_ft, pinhole_positions=pinhole_positions, ex2p=ex2p)
+    return confocal_int(psf_ex, psf_em, pp_em; pinhole_pix=pinhole_pix, pinhole_ft=pinhole_ft, pinhole_positions=pinhole_positions, ex_modifier=ex_modifier)
 end
 
 
